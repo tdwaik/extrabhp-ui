ef81e6ac744ccc7fb3e3a251a120fc42a90eebac
Thaer Aldwaik <taldwaik@souq.com>
2017-10-31 17:54:29 +0200

    ishelpful & feedback

--- app/controller/homeController.js
+++ app/controller/homeController.js
@@ -2,13 +2,16 @@
  * @author Thaer Aldwaik <t_dwaik@hotmail.com>
  */
 
-app.controller('homeController', ['$scope', 'alerts', 'whatCarService', function($scope, alerts, whatCarService) {
+app.controller('homeController', ['$scope', '$timeout', 'alerts', 'whatCarService', function($scope, $timeout, alerts, whatCarService) {
 
     $scope.answers = [];
     $scope.question = null;
     $scope.result = null;
     $scope.checkboxModel = {};
     $scope.loading = true;
+    $scope.showThanks = false;
+
+    var whatCarLogId = null;
 
     $scope.getQuestion = function () {
         $scope.loading = true;
@@ -22,8 +25,9 @@ app.controller('homeController', ['$scope', 'alerts', 'whatCarService', function
                 }
                 $scope.loading = false;
             }else if(response.status === 201) {
-                $scope.result = response.data;
+                $scope.result = response.data.result;
                 $scope.loading = false;
+                whatCarLogId = response.data.whatCarLogId;
             }else {
                 alerts.oops();
             }
@@ -56,16 +60,25 @@ app.controller('homeController', ['$scope', 'alerts', 'whatCarService', function
     };
 
     $scope.init = function() {
-        $scope.getQuestion();
-    };
-
-    $scope.tryAgain = function() {
         $scope.answers = [];
         $scope.question = null;
         $scope.result = null;
         $scope.checkboxModel = {};
         $scope.loading = true;
 
+        $scope.isHelpful.show = true;
+        $scope.isHelpful.loading = false;
+
+        $scope.feedback.show = false;
+        $scope.feedback.loading = false;
+        $scope.feedback.value = null;
+
+        $scope.showThanks = false;
+
+        $scope.getQuestion();
+    };
+
+    $scope.tryAgain = function() {
         $scope.init();
     };
 
@@ -74,8 +87,35 @@ app.controller('homeController', ['$scope', 'alerts', 'whatCarService', function
         "loading": false,
         "send": function (isHelpful) {
             $scope.isHelpful.loading = true;
-            // TODO send feedback
+            whatCarService.sendIsHelpful(whatCarLogId, isHelpful).then(function(response) {
+                $scope.isHelpful.loading = false;
+                $scope.isHelpful.show = false;
+                $scope.feedback.show = true;
+            }, function (response) {
+                alerts.oops();
+            });
         }
     };
+
+    $scope.feedback = {
+        "show": false,
+        "loading": false,
+        "value": null,
+        "send": function () {
+            $scope.feedback.loading = true;
+            whatCarService.sendFeedback(whatCarLogId, $scope.feedback.value).then(function(response) {
+                $scope.feedback.loading = false;
+                $scope.feedback.show = false;
+
+                $scope.showThanks = true;
+                $timeout(function() {
+                    $scope.showThanks = false;
+                }, 1000);
+
+            }, function (response) {
+                alerts.oops();
+            });
+        }
+    }
 
 }]);
--- app/services.js
+++ app/services.js
@@ -6,11 +6,12 @@ var APIEndPoint = 'http://localhost:8080';
 
 app.service('API', ['$http', function($http) {
 	return {
-		call: function(method, url, data) {
+		call: function(method, url, data, headers) {
 			return $http({
 				method: method,
 				url: APIEndPoint + url,
-				data: data
+				data: data,
+                headers: headers
 			});
 		}
 	}
@@ -19,5 +20,15 @@ app.service('API', ['$http', function($http) {
 app.service('whatCarService', ['API', function(API) {
     this.getQuestion = function(answers) {
         return API.call('post', '/whatCar', answers);
+    };
+    this.sendIsHelpful = function(whatCarLogId, isHelpful) {
+    	var jsonRequest = {"whatCarLogId": whatCarLogId, "isHelpful": isHelpful};
+    	var headers = {"Content-Type": "application/json"};
+        return API.call('post', '/whatCar/isHelpful', JSON.stringify(jsonRequest), headers);
+	};
+	this.sendFeedback = function(whatCarLogId, feedback) {
+        var jsonRequest = {"whatCarLogId": whatCarLogId, "feedback": feedback};
+        var headers = {"Content-Type": "application/json"};
+        return API.call('post', '/whatCar/feedback', JSON.stringify(jsonRequest), headers);
     };
 }]);
