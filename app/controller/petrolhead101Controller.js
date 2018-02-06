7db3a3e88664fb811253d00cfef33f49c22ecde5
Thaer Aldwaik <taldwaik@souq.com>
2018-02-06 18:45:55 +0200

    new footer

--- app/controller/petrolhead101Controller.js
+++ app/controller/petrolhead101Controller.js
@@ -9,6 +9,15 @@ app.controller('petrolhead101Controller', ['$scope', '$anchorScroll', function($
         {code: 'Differential', img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/BAUMA_2004_ZF_Differentialgetriebe.jpg/1920px-BAUMA_2004_ZF_Differentialgetriebe.jpg', title: 'Differential', content: 'Differential used to control the take-up reel of a paper tape reader made by Tally circa 1962. The bevel gears spin freely on their shafts, unless a brake shoe stops the left gear. This causes the planet gear to drive the output shaft at half the speed of the driven gear on the right.', ref: 'Wikipedia', ref_link: 'https://en.wikipedia.org/wiki/Differential_(mechanical_device)'},
         {code: 'Limited-slip_differential', img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8b/Limited_slip_diff_cone.jpg/1280px-Limited_slip_diff_cone.jpg', title: 'Limited-slip differential', content: 'A limited-slip differential is a type of differential that allows its two output shafts to rotate at different speeds but limits the maximum difference between the two shafts.', ref: 'Wikipedia', ref_link: 'https://en.wikipedia.org/wiki/Limited-slip_differential'},
         {code: 'Mazda_Wankel_engine', img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Wankel-1.jpg/560px-Wankel-1.jpg', title: 'Mazda Wankel engine', content: 'The Mazda Wankel engines comprise a family of car engines produced by Mazda, derived from experiments in the early 1960s by Felix Wankel, a German engineer. Over the years, displacement has been increased and turbocharging has been added.', ref: 'Wikipedia', ref_link: 'https://en.wikipedia.org/wiki/Mazda_Wankel_engine'}
-    ]
+    ];
+
+    $scope.dictionary = [
+        {code: "Petrolhead", name: "Petrolhead", content: "Car Enthusiast"},
+        {code: "JDM", name: "JDM", content: "Japanese domestic market"},
+        {code: "Muscle_car", name: "Muscle Car", content: "American term used to refer to a variety of high-performance automobiles"},
+        {code: "ABS", name: "ABS (Anti-lock braking system)", content: "An anti-lock braking system or anti-skid braking system (ABS) is an automobile safety system that allows the wheels on a motor vehicle to maintain tractive contact with the road surface according to driver inputs while braking, preventing the wheels from locking up (ceasing rotation) and avoiding uncontrolled skidding."},
+        {code: "ESC", name: "ESC / ESP / DSC / ASC", content: "Electronic stability control (ESC), also referred to as electronic stability program (ESP) or dynamic stability control (DSC), is a computerized technology that improves a vehicle's stability by detecting and reducing loss of traction (skidding). When ESC detects loss of steering control, it automatically applies the brakes to help \"steer\" the vehicle where the driver intends to go. Braking is automatically applied to wheels individually, such as the outer front wheel to counter oversteer or the inner rear wheel to counter understeer. Some ESC systems also reduce engine power until control is regained. ESC does not improve a vehicle's cornering performance; instead, it helps to minimize the loss of control."},
+        {code: "Powertrain", name: "Powertrain", content: "The mechanism that transmits the drive from the engine of a vehicle to its axle."}
+    ];
 
 }]);
