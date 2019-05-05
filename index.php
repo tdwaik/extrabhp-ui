9515e3da16886534165998039e87a8c48e8b6d9a
Thaer Aldwaik <tdwaik@hotmail.com>
2019-05-05 17:16:41 +0300

    new footer style

--- index.php
+++ index.php
@@ -48,7 +48,7 @@
     <meta name="description" content="Extra BHP, Cars Enthusiasts Community" />
     <meta name="author" content="Thaer Aldwaik" />
     <meta name="fb:app_id" content="1959489680985703" />
-    <!-- <meta name="theme-color" content="#000" /> -->
+    <meta name="theme-color" content="#000" />
 
     <base href="/" />
 
@@ -62,7 +62,7 @@
 
     <!-- Bootstrap Core CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 
     <!-- Custom Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet" />
